221e367739bea973a26866037f5fbdf1cfff9df4
DM <session2@START-Coding-MBP-16.local>
2016-02-26 18:08:17 -0700

    Not functional, but the main purpose is to facilitate the accessibility of social media, it should have allowed for you to click on certain button that links it to your social media page.

---
+++ popup.js
@@ -0,0 +1,26 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback - called when the URL of the current tab
+ *   is found.
+ */
+/**
+ * Created by session2 on 2/19/16.
+ */
+console.log('it works');
+function changeTheme(choice){
+    if (choice === "green") {
+        document.body.style.backgroundColor = "green";
+    }
+}
+//document.getElementById('main-content').style.backgroundColor = "red";
+document.body.style.backgroundColor = "green";
+//document.body.innerHTML = 'ABCDEFG';/**
+ * Created by session2 on 2/23/16.
+ */
+
+
